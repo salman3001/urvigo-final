8cfebefe6f01744591cecb4a80109c9fdd61f057
salman <therodfighter@gmail.com>
2024-04-20 20:05:27 +0400

    UPDATES

---
+++ pre_build.js
@@ -0,0 +1,17 @@
+import { promises as fsPromises, constants } from 'node:fs'
+
+const directoryPath = 'build'
+
+// Check if the directory exists
+async function checkAndDeleteDirectory(path) {
+  try {
+    await fsPromises.access(directoryPath, constants.F_OK)
+    // Directory exists, so delete it
+    await fsPromises.rm(directoryPath, { recursive: true })
+    console.log('Directory deleted successfully')
+  } catch (err) {
+    console.error('Error:', err.code === 'ENOENT' ? 'Directory does not exist' : err.message)
+  }
+}
+
+checkAndDeleteDirectory(directoryPath)
