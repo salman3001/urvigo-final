4e8eb0ebe3a89ce6e9f2c9c30bf945424e1ab844
salman <therodfighter@gmail.com>
2024-04-29 23:33:45 +0400

    deleted old routes

--- inertia/utils/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  auth: {
-    login: '/auth/login',
-    logout: '/auth/logout',
-    sign_up: '/auth/signup',
-    forgot_password: '/auth/forgot-password',
-    reset_password: '/auth/reset-password',
-  },
-}
